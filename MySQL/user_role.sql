291b4910be3c415a59883666433ccb9920d88423
Intvarinit <2015546239@qq.com>
2025-06-27 18:03:28 +0800

    增加了数据库结构以及表记录的信息

---
+++ MySQL/user_role.sql
@@ -0,0 +1,3 @@
+INSERT INTO huinong_platform.user_role (id, user_type, role_code, create_time) VALUES (1, 1, 'admin', '2025-06-18 23:24:05');
+INSERT INTO huinong_platform.user_role (id, user_type, role_code, create_time) VALUES (2, 3, 'inspector', '2025-06-18 23:24:05');
+INSERT INTO huinong_platform.user_role (id, user_type, role_code, create_time) VALUES (3, 2, 'user', '2025-06-18 23:24:05');
